982e694a73d2c4a85b11f9b53a17bb1e9fcb0ff7
unknown <gblock@GBMACWIN.redmond.corp.microsoft.com>
2012-01-14 14:34:19 -0500

    modified

--- server.js
+++ server.js
@@ -2,5 +2,5 @@ var http = require('http');
 var port = process.env.port || 1337;
 http.createServer(function (req, res) {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello World - git update\n');
+    res.end('Hello World - pulled from Github\n');
 }).listen(port);
